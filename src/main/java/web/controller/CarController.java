4955be5319bc194758a61ebbbf02ff472219b4d5
SafarovFarid97 <safarovfarid97@mail.ru>
2023-04-19 22:57:29 +0300

    SafarovFarid

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,34 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+
+
+@Controller
+public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+
+    @GetMapping("/cars")
+    public String countedCars(@RequestParam(name = "count", required = false) String str, ModelMap model) {
+        if (str != null) {
+            int count = Integer.parseInt(str);
+            model.addAttribute("cars", carService.counted(count));
+        } else {
+            model.addAttribute("cars", carService.all());
+        }
+        return "car";
+    }
+
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,32 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarServiceImpl implements CarService {
+
+    private List<Car> cars;
+
+
+    CarServiceImpl() {
+        cars = new ArrayList<>();
+        cars.add(new Car(1,"UAZ",469));
+        cars.add(new Car(2,"GAZ",352));
+        cars.add(new Car(3,"VAZ",128));
+        cars.add(new Car(4,"TOYOTA",25));
+        cars.add(new Car(5,"BMW",525));
+    }
+
+    public List<Car> all() {
+        return cars;
+    }
+
+    public List<Car> counted(int count) {
+        return cars.stream().limit(count).toList();
+    }
+
+}
